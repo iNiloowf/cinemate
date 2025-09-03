ffbcc1ea8b78141940da2a1fb7af0ac2e7ce483f
iNiloowf <niloowf@gmail.com>
2025-09-03 13:01:24 -0600

    card component added

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,3 +1,17 @@
+import { Card } from "../components/Card";
+
 export const MovieList = () => {
-  return <main>MovieList</main>;
+  return (
+    <main>
+      <section>
+        <div className="flex justify-start flex-wrap">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </section>
+    </main>
+  );
 };
