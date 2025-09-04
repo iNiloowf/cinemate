1f89132dcb7a604ed7071d92cad4b5f127794b7a
iNiloowf <niloowf@gmail.com>
2025-09-04 15:12:29 -0600

    api keys + fetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+export const useFetch = (apiPath) => {
+  const [data, setData] = useState([]);
+  const url = `https://api.themoviedb.org/3/movie/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`;
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json.results);
+    }
+    fetchMovies();
+  }, [url]);
+  return { data };
+};
